3190328b3a19bc13a2678c780a622287d44b51a1
SaffronWilliamson <saffron.havana99@gmail.com>
2025-02-19 14:55:06 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ function App() {
       <header className="App-header">
         <h1>Weather Search Engine</h1>
         <Weather city="Manchester" />
+        <p>
+          Coded by
+          <a href="https://github.com/SaffronWilliamson"> Saffron </a>
+          open sourced on{' '}
+          <a href="https://github.com/SaffronWilliamson/weather-react">
+            GitHub
+          </a>
+          , and hosted on
+          <a href="https://weather-react-saffron.netlify.app/"> Netlify</a>
+        </p>
       </header>
-      
     </div>
   );
 }
